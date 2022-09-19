c15a2743545c23b657c8dd39411e29156e70042a
Crasow <olariliya2003@gmail.com>
2022-09-20 00:54:58 +0300

    Initial commit for third lesson

--- mainapp/views.py
+++ mainapp/views.py
@@ -14,10 +14,17 @@ class NewsPageView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["news_title"] = "Громкий новостной заголовок"
         context['news_preview'] = 'Предварительное описание, чтоб заманить читателя'
-        context['range'] = range(5)
+        context['pages_range'] = range(1, 5)
+        context['article_range'] = range(5)
         context["datetime_obj"] = datetime.now()
         return context
 
+
+class NewsWithPaginatorView(NewsPageView):
+    def get_context_data(self, page, **kwargs):
+        context = super().get_context_data(page=page, **kwargs)
+        context["page_num"] = page
+        return context
 
 class CoursesPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
