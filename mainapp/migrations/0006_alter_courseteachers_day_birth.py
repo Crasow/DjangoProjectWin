b00eb6cf25080ea30ca9b3c07271ea304054ce7c
Crasow <olariliya2003@gmail.com>
2022-09-26 18:31:43 +0300

    Initial commit for fifth lesson

--- mainapp/migrations/0006_alter_courseteachers_day_birth.py
+++ mainapp/migrations/0006_alter_courseteachers_day_birth.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mainapp', '0005_course_teachers_migration'),
+        ('mainapp', '0003_courses_migration'),
     ]
 
     operations = [
